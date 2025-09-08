ee47f968be91dbd68908d61f19eb3c9825667ea3
Viktor Chupak <50545334+vikchupak@users.noreply.github.com>
2025-09-08 17:11:51 +0300

    Add script template

---
+++ scripts/print-message.ts
@@ -0,0 +1,17 @@
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "src/app.module";
+import { AppService } from "src/app.service";
+
+async function bootstrap() {
+  const app = await NestFactory.createApplicationContext(AppModule);
+
+  const appService = app.get(AppService);
+
+  const message = appService.getHello();
+
+  console.log(`✅ Message from script: ${message}`);
+
+  await app.close();
+}
+
+void bootstrap();
