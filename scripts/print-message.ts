6b74c6ce1d8a49abef035d6f608b22a629dde956
Viktor Chupak <viktor.ch@golaunchlabs.com>
2025-09-13 14:39:58 +0300

    Add scripts folder to eslint

--- scripts/print-message.ts
+++ scripts/print-message.ts
@@ -1,6 +1,6 @@
-import { NestFactory } from "@nestjs/core";
-import { AppModule } from "src/app.module";
-import { AppService } from "src/app.service";
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from 'src/app.module';
+import { AppService } from 'src/app.service';
 
 async function bootstrap() {
   const app = await NestFactory.createApplicationContext(AppModule);
